54b7584873fea0127c8a7e9eafb1ea77701bf1a8
yuanze31 <1538981620@qq.com>
2024-02-03 22:29:12 +0800

    规范格式

--- data/scripts/RotateObject.lua
+++ data/scripts/RotateObject.lua
@@ -1,316 +1,315 @@
 -- RotateObject --
-
-
 function look_around(types, cb, range, justOne)
-	local count = 0
-	if range == "max" then range = 9999 end
-	for k, v in pairs(types) do
-		local os = this.GetNearbyObjects(v, (range or nearbyRange))
-		for o, distance in pairs( os ) do
-			count = count + 1
-			if cb then
-				cb(o, distance)
-			end
-			if justOne then return o end
-		end
-	end
-	if justOne then
-		return nil
-	else
-		return count
-	end
+    local count = 0
+    if range == "max" then
+        range = 9999
+    end
+    for k, v in pairs(types) do
+        local os = this.GetNearbyObjects(v, (range or nearbyRange))
+        for o, distance in pairs(os) do
+            count = count + 1
+            if cb then
+                cb(o, distance)
+            end
+            if justOne then
+                return o
+            end
+        end
+    end
+    if justOne then
+        return nil
+    else
+        return count
+    end
 end
-
-
-
 
 function Create()
-	rotateThose()
-	this.Hidden = true
-	this.Delete()
+    rotateThose()
+    this.Hidden = true
+    this.Delete()
 end
 
-local wallTypeDict = {ConcreteWall=true, BrickWall=true, BurntWall=true, Fence=true, PerimeterWall=true}
+local wallTypeDict = {
+    ConcreteWall = true,
+    BrickWall = true,
+    BurntWall = true,
+    Fence = true,
+    PerimeterWall = true
+}
 
-
-local objTypes = {'PersonalSink', 'MedicineCabinet',  'AccountingTable', "Bed", "BunkBed", "Crib", "PlayMat", "Toilet", "Table", "Chair", "Bench", "JailDoor", "JailDoorLarge", "JailDoorXL","Door", "StaffDoor", "SolitaryDoor", "RemoteDoor", "JailBars", "Window", "WindowLarge", "Cooker", "Fridge", "Bin", "Sink", "ServingTable", "ShowerHead", "Bookshelf", "OfficeDesk", "SchoolDesk", "FilingCabinet", "MedicalBed", "MorgueSlab", "PowerSwitch", "WaterBoiler", "MetalDetector", "WeightsBench", "PhoneBooth", "Tv", "LargeTv", "PoolTable", "Cctv", "CctvMonitor", "DoorControlSystem", "PhoneMonitor", "Servo", "DoorTimer", "LogicCircuit", "IroningBoard", "VisitorTable", "VisitorTableSecure", "DogCrate", "GuardLocker", "SofaChairSingle", "SofaChairDouble", "DrinkMachine", "ArcadeCabinet", "Radio", "Altar", "Pews", "PrayerMat", "LibraryBookshelf", "SortingTable", "ShopFront", "ShopShelf", "GuardTower", "WeaponRack", "Radiator"}
+local objTypes = {'PersonalSink', 'MedicineCabinet', 'AccountingTable', "Bed", "BunkBed", "Crib", "PlayMat", "Toilet",
+                  "Table", "Chair", "Bench", "JailDoor", "JailDoorLarge", "JailDoorXL", "Door", "StaffDoor",
+                  "SolitaryDoor", "RemoteDoor", "JailBars", "Window", "WindowLarge", "Cooker", "Fridge", "Bin", "Sink",
+                  "ServingTable", "ShowerHead", "Bookshelf", "OfficeDesk", "SchoolDesk", "FilingCabinet", "MedicalBed",
+                  "MorgueSlab", "PowerSwitch", "WaterBoiler", "MetalDetector", "WeightsBench", "PhoneBooth", "Tv",
+                  "LargeTv", "PoolTable", "Cctv", "CctvMonitor", "DoorControlSystem", "PhoneMonitor", "Servo",
+                  "DoorTimer", "LogicCircuit", "IroningBoard", "VisitorTable", "VisitorTableSecure", "DogCrate",
+                  "GuardLocker", "SofaChairSingle", "SofaChairDouble", "DrinkMachine", "ArcadeCabinet", "Radio",
+                  "Altar", "Pews", "PrayerMat", "LibraryBookshelf", "SortingTable", "ShopFront", "ShopShelf",
+                  "GuardTower", "WeaponRack", "Radiator"}
 -- Except "PowerStation", "Capacitor", "WaterPumpStation", "ElectricChair", "LogicBridge", "StatusLight", "PipeValve", "Light", "Sprinkler", "Drain", "WorkshopSaw", "WorkshopPress", "CarpenterTable", "PressurePad", "LaundryMachine",
 
-
-
 sizeDict = {
-	AccountingTable = {3, 1},
-	Table = {4, 1},
-	Bench = {4, 1},
-	Bed = {1, 2},
-	BunkBed = {1, 2},
-	SuperiorBed = {1, 2},
-	MedicalBed = {2, 2},
-	ElectricChair = {2, 2},
-	JailDoorXL = {3, 1},
-	JailDoorLarge = {2, 1},
-	WindowLarge = {2, 1},
-	Cooker = {2, 1},
-	Fridge = {2, 1},
-	Sink = {3, 1},
-	ServingTable = {5, 1},
-	OfficeDesk = {2, 1},
-	WaterBoiler = {2, 1},
-	LargeTv = {3, 1},
-	ShopFront = {2, 1},
-	CctvMonitor = {3, 1},
-	DoorControlSystem = {2, 1},
-	WeaponRack = {2, 1},
-	SofaChairDouble = {2, 1},
-	SchoolDesk = {1, 2},
-	MorgueSlab = {1, 2},
-	PowerStation = {3, 3},
-	WaterPumpStation = {3, 3},
-	Servo = {1, 2},
-	PoolTable = {3, 2},
-	WorkshopSaw = {3, 1},
-	WorkshopPress = {3, 1},
-	CarpenterTable = {3, 2},
-	VisitorTable = {3, 2},
-	VisitorTableSecure = {2, 2},
-	IroningBoard = {3, 1},
-	SortingTable = {3, 1},
-	LibraryBookshelf = {3, 1},
-	ShopShelf = {3, 1},
-	Altar = {3, 2},
-	Pews = {4, 1},
-	DogCrate = {1, 2},
-	GuardTower = {2, 2},
-	WaterBoiler = {2, 1},
+    AccountingTable = {3, 1},
+    Table = {4, 1},
+    Bench = {4, 1},
+    Bed = {1, 2},
+    BunkBed = {1, 2},
+    SuperiorBed = {1, 2},
+    MedicalBed = {2, 2},
+    ElectricChair = {2, 2},
+    JailDoorXL = {3, 1},
+    JailDoorLarge = {2, 1},
+    WindowLarge = {2, 1},
+    Cooker = {2, 1},
+    Fridge = {2, 1},
+    Sink = {3, 1},
+    ServingTable = {5, 1},
+    OfficeDesk = {2, 1},
+    WaterBoiler = {2, 1},
+    LargeTv = {3, 1},
+    ShopFront = {2, 1},
+    CctvMonitor = {3, 1},
+    DoorControlSystem = {2, 1},
+    WeaponRack = {2, 1},
+    SofaChairDouble = {2, 1},
+    SchoolDesk = {1, 2},
+    MorgueSlab = {1, 2},
+    PowerStation = {3, 3},
+    WaterPumpStation = {3, 3},
+    Servo = {1, 2},
+    PoolTable = {3, 2},
+    WorkshopSaw = {3, 1},
+    WorkshopPress = {3, 1},
+    CarpenterTable = {3, 2},
+    VisitorTable = {3, 2},
+    VisitorTableSecure = {2, 2},
+    IroningBoard = {3, 1},
+    SortingTable = {3, 1},
+    LibraryBookshelf = {3, 1},
+    ShopShelf = {3, 1},
+    Altar = {3, 2},
+    Pews = {4, 1},
+    DogCrate = {1, 2},
+    GuardTower = {2, 2}
 }
 
-
-
-
-
-
-
-
-local orDirs = {
-	{0, 1},
-	{-1, 0},
-	{0, -1},
-	{1, 0},
-	{0, 1},
-}
+local orDirs = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}}
 
 function rotateDirOf(o, orGroup)
-	for i, xy in pairs( orDirs ) do
+    for i, xy in pairs(orDirs) do
 
-		if i > 4 then
-			Game.DebugOut('Error: orGroup is invalid')
-		else
+        if i > 4 then
+            Game.DebugOut('Error: orGroup is invalid')
+        else
 
-			local dx = xy[1]
-			local dy = xy[2]
-			if dx == o[orGroup].x and dy == o[orGroup].y then
-				local newOr = orDirs[i+1]
-				-- print('from')
-				-- print(o[orGroup].x)
-				-- print(o[orGroup].y)
-				-- print('to')
-				-- print(newOr[1])
-				-- print(newOr[2])
-				-- If not `* 1.01`, sometimes the image will not change as rotating happens.
-				if orGroup == 'Or' then
-					o[orGroup].x = newOr[1]
-					-- o[orGroup].y = newOr[2] * 2
-					o[orGroup].y = newOr[2] * 1.01
-				else
-					o[orGroup].x = newOr[1] * 1.01
-					o[orGroup].y = newOr[2]
-				end
-				return
-			end
+            local dx = xy[1]
+            local dy = xy[2]
+            if dx == o[orGroup].x and dy == o[orGroup].y then
+                local newOr = orDirs[i + 1]
+                -- print('from')
+                -- print(o[orGroup].x)
+                -- print(o[orGroup].y)
+                -- print('to')
+                -- print(newOr[1])
+                -- print(newOr[2])
+                -- If not `* 1.01`, sometimes the image will not change as rotating happens.
+                if orGroup == 'Or' then
+                    o[orGroup].x = newOr[1]
+                    -- o[orGroup].y = newOr[2] * 2
+                    o[orGroup].y = newOr[2] * 1.01
+                else
+                    o[orGroup].x = newOr[1] * 1.01
+                    o[orGroup].y = newOr[2]
+                end
+                return
+            end
 
-		end
+        end
 
-	end
+    end
 end
 
 function offsetO(o)
 
-	local rx = o.Pos.x - this.Pos.x
-	local ry = o.Pos.y - this.Pos.y
-	if rx == 0 and ry == 0 then return end
+    local rx = o.Pos.x - this.Pos.x
+    local ry = o.Pos.y - this.Pos.y
+    if rx == 0 and ry == 0 then
+        return
+    end
 
-	local size = sizeDict[o.Type]
-	if size == nil then
-		print('No offset about such type:')
-		print(o.Type)
-		o.Pos.x = o.Pos.x + 0.0001 * o.Or.x
-		return
-	end
-	if (size[1] + size[2]) % 2 == 0 then
-		o.Pos.y = o.Pos.y + 0.0001 * o.Or.y
-		return
-	end
+    local size = sizeDict[o.Type]
+    if size == nil then
+        print('No offset about such type:')
+        print(o.Type)
+        o.Pos.x = o.Pos.x + 0.0001 * o.Or.x
+        return
+    end
+    if (size[1] + size[2]) % 2 == 0 then
+        o.Pos.y = o.Pos.y + 0.0001 * o.Or.y
+        return
+    end
 
-	print('offset')
-	print(rx)
-	print(ry)
+    print('offset')
+    print(rx)
+    print(ry)
 
-	rx = rx * 2
-	ry = ry * 2
+    rx = rx * 2
+    ry = ry * 2
 
-	for i, xy in pairs( orDirs ) do
+    for i, xy in pairs(orDirs) do
 
-		if i > 4 then
-			Game.DebugOut('Error: rx an ry is invalid')
-		else
+        if i > 4 then
+            Game.DebugOut('Error: rx an ry is invalid')
+        else
 
-			local dx = xy[1]
-			local dy = xy[2]
-			if dx == rx and dy == ry then
-				local newOr = orDirs[i+1]
-				-- print('from')
-				-- print(rx)
-				-- print(ry)
-				-- print('to')
-				-- print(newOr[1])
-				-- print(newOr[2])
-				ry = newOr[2] / 2
-				rx = newOr[1] / 2
-				break
-			end
+            local dx = xy[1]
+            local dy = xy[2]
+            if dx == rx and dy == ry then
+                local newOr = orDirs[i + 1]
+                -- print('from')
+                -- print(rx)
+                -- print(ry)
+                -- print('to')
+                -- print(newOr[1])
+                -- print(newOr[2])
+                ry = newOr[2] / 2
+                rx = newOr[1] / 2
+                break
+            end
 
-		end
+        end
 
-	end
-	print(rx)
-	print(ry)
-	o.Pos.x = this.Pos.x + rx
-	o.Pos.y = this.Pos.y + ry
+    end
+    print(rx)
+    print(ry)
+    o.Pos.x = this.Pos.x + rx
+    o.Pos.y = this.Pos.y + ry
 
-	-- if o.Type == 'Pews' then
-	-- 	if o.Or.x == 0 then
-	-- 		-- vertical
-	-- 		print('vertical')
-	-- 		o.Pos.y = o.Pos.y + 0.5
-	-- 		o.Pos.x = o.Pos.x + 0.5
-	-- 	else
-	-- 		o.Pos.y = o.Pos.y - 0.5
-	-- 		o.Pos.x = o.Pos.x - 0.5
-	-- 		-- o.Pos.y = o.Pos.y - 0.5
-	-- 		-- o.Pos.x = o.Pos.x - 0.5
-	-- 	end
-	-- end
+    -- if o.Type == 'Pews' then
+    -- 	if o.Or.x == 0 then
+    -- 		-- vertical
+    -- 		print('vertical')
+    -- 		o.Pos.y = o.Pos.y + 0.5
+    -- 		o.Pos.x = o.Pos.x + 0.5
+    -- 	else
+    -- 		o.Pos.y = o.Pos.y - 0.5
+    -- 		o.Pos.x = o.Pos.x - 0.5
+    -- 		-- o.Pos.y = o.Pos.y - 0.5
+    -- 		-- o.Pos.x = o.Pos.x - 0.5
+    -- 	end
+    -- end
 end
 
 function rotateO(o)
-	o.Or.x = math.min(o.Or.x, 1)
-	o.Or.x = math.max(o.Or.x, -1)
-	o.Or.y = math.min(o.Or.y, 1)
-	o.Or.y = math.max(o.Or.y, -1)
-	rotateDirOf(o, 'Or')
-	offsetO(o)
+    o.Or.x = math.min(o.Or.x, 1)
+    o.Or.x = math.max(o.Or.x, -1)
+    o.Or.y = math.min(o.Or.y, 1)
+    o.Or.y = math.max(o.Or.y, -1)
+    rotateDirOf(o, 'Or')
+    offsetO(o)
 
-	print('now-----')
-	print(o.Or.x)
-	print(o.Or.y)
-	if o.OpenDir then
-		rotateDirOf(o, 'OpenDir')
-	end
-	updateWall(o)
+    print('now-----')
+    print(o.Or.x)
+    print(o.Or.y)
+    if o.OpenDir then
+        rotateDirOf(o, 'OpenDir')
+    end
+    updateWall(o)
 end
 
 function rotateThose()
 
-	for k, v in pairs(objTypes) do
-		local os = this.GetNearbyObjects(v, 0.75)
-		for o, distance in pairs( os ) do
-			if o.Hidden then
-			else
-				rotateO(o)
-				this.Sound("Guard", "Place") -- Fffoww
-			end
-		end
-	end
+    for k, v in pairs(objTypes) do
+        local os = this.GetNearbyObjects(v, 0.75)
+        for o, distance in pairs(os) do
+            if o.Hidden then
+            else
+                rotateO(o)
+                this.Sound("Guard", "Place") -- Fffoww
+            end
+        end
+    end
 end
-
-
 
 function tryFindNewWall(o)
-	-- print('tryFindNewWall')
-	o.Walls.x = 0
-	o.Walls.y = 0
-	local dir = {}
-	local cellOr = {}
-	for i, xy in pairs( orDirs ) do
+    -- print('tryFindNewWall')
+    o.Walls.x = 0
+    o.Walls.y = 0
+    local dir = {}
+    local cellOr = {}
+    for i, xy in pairs(orDirs) do
 
-		if i > 4 then break end
+        if i > 4 then
+            break
+        end
 
-		local dx = xy[1]
-		local dy = xy[2]
-		-- print(dx)
-		-- print(dy)
-		-- print('...')
-		if dx == o.Walls.x and dy == o.Walls.y then
-		else
-			local cell = getCell(o.Pos.x + dx, o.Pos.y + dy)
-			if wallTypeDict[cell.Mat] then
-				-- print('NEW WALL!')
-				-- print(dx)
-				-- print(dy)
-				o.Walls.x = dx
-				o.Walls.y = dy
-				-- dir = {
-				-- 	x = dx,
-				-- 	y = dy,
-				-- }
-				if dx == o.Or.x and dy == o.Or.y then
-					print('o.Or.y')
-					print(o.Or.y)
-					-- cellOr = {
-					-- 	x = dx,
-					-- 	y = dy,
-					-- }
-					break
-				end
-			end
-		end
-	end
-	-- if dir.x == nil then
-	-- else
-	-- end
+        local dx = xy[1]
+        local dy = xy[2]
+        -- print(dx)
+        -- print(dy)
+        -- print('...')
+        if dx == o.Walls.x and dy == o.Walls.y then
+        else
+            local cell = getCell(o.Pos.x + dx, o.Pos.y + dy)
+            if wallTypeDict[cell.Mat] then
+                -- print('NEW WALL!')
+                -- print(dx)
+                -- print(dy)
+                o.Walls.x = dx
+                o.Walls.y = dy
+                -- dir = {
+                -- 	x = dx,
+                -- 	y = dy,
+                -- }
+                if dx == o.Or.x and dy == o.Or.y then
+                    print('o.Or.y')
+                    print(o.Or.y)
+                    -- cellOr = {
+                    -- 	x = dx,
+                    -- 	y = dy,
+                    -- }
+                    break
+                end
+            end
+        end
+    end
+    -- if dir.x == nil then
+    -- else
+    -- end
 end
-
 
 function makeBooleanDict(t)
-	local d = {}
-	for k, v in pairs(t) do
-		d[v] = true
-	end
-	return d
+    local d = {}
+    for k, v in pairs(t) do
+        d[v] = true
+    end
+    return d
 end
 
-local AttachToWallTypes = {'Radio', 'Crib', 'Toilet', 'Table', 'ShowerHead', 'Bookshelf', 'OfficeDesk', 'FilingCabinet', 'MorgueSlab', 'PhoneBooth', 'Tv', 'Cctv', 'GuardLocker', 'SofaChairSingle', 'SofaChairDouble', 'DrinkMachine', 'ArcadeCabinet', 'Altar', 'Radiator',}
+local AttachToWallTypes = {'Radio', 'Crib', 'Toilet', 'Table', 'ShowerHead', 'Bookshelf', 'OfficeDesk', 'FilingCabinet',
+                           'MorgueSlab', 'PhoneBooth', 'Tv', 'Cctv', 'GuardLocker', 'SofaChairSingle',
+                           'SofaChairDouble', 'DrinkMachine', 'ArcadeCabinet', 'Altar', 'Radiator'}
 -- 'Bed', 'SuperiorBed', 'SchoolDesk', 'Sprinkler', 'WeaponRack',
 local AttachToWallTypeDict = makeBooleanDict(AttachToWallTypes)
 
 function updateWall(o)
-	if not AttachToWallTypeDict[o.Type] then
-		return
-	end
+    if not AttachToWallTypeDict[o.Type] then
+        return
+    end
 
-	if o.Walls.x == 0 and o.Walls.y == 0 then
-		tryFindNewWall(o)
-	else
-		local cellWall = getCell(o.Pos.x + o.Walls.x, o.Pos.y + o.Walls.y)
-		-- print(cellWall.Mat)
-		if not wallTypeDict[cellWall.Mat] then
-			tryFindNewWall(o)
-		end
-	end
+    if o.Walls.x == 0 and o.Walls.y == 0 then
+        tryFindNewWall(o)
+    else
+        local cellWall = getCell(o.Pos.x + o.Walls.x, o.Pos.y + o.Walls.y)
+        -- print(cellWall.Mat)
+        if not wallTypeDict[cellWall.Mat] then
+            tryFindNewWall(o)
+        end
+    end
 end
 
 function getCell(x, y)
-	return World.GetCell(math.floor(x), math.floor(y))
+    return World.GetCell(math.floor(x), math.floor(y))
 end
-
 
--- data/world.lua
+++ data/world.lua
@@ -1,40 +1,42 @@
 -- world.lua --
-
 function look(who, types, cb, range, justOne)
-	local count = 0
-	if range == "max" then range = 9999 end
+    local count = 0
+    if range == "max" then
+        range = 9999
+    end
 
-	for k, v in pairs(types) do
-		local os = who.GetNearbyObjects(v, range)
-		for o, distance in pairs( os ) do
-			-- print(o.Type)
-			count = count + 1
-			if cb then
-				cb(o)
-			end
-			if justOne then return o end
-		end
-	end
+    for k, v in pairs(types) do
+        local os = who.GetNearbyObjects(v, range)
+        for o, distance in pairs(os) do
+            -- print(o.Type)
+            count = count + 1
+            if cb then
+                cb(o)
+            end
+            if justOne then
+                return o
+            end
+        end
+    end
 
-	if justOne then
-		return nil
-	else
-		return count
-	end
+    if justOne then
+        return nil
+    else
+        return count
+    end
 end
 
-
 function deleteO(o)
-	-- print('rs !!!!!!!!!')
-	o.Delete()
+    -- print('rs !!!!!!!!!')
+    o.Delete()
 end
 
 local anchor
 
 function deleteOldAnchor(o)
-	if o.Id.u ~= anchor.Id.u then
-		o.Delete()
-	end
+    if o.Id.u ~= anchor.Id.u then
+        o.Delete()
+    end
 end
 
 local Ready = 0
@@ -42,80 +44,76 @@ local Ready2 = 0
 
 function Update(timePassed)
 
-	if anchor and anchor.Damage == nil then
-		anchor = nil
-	end
+    if anchor and anchor.Damage == nil then
+        anchor = nil
+    end
 
-	if not anchor then
-		anchor = Object.Spawn('Anchor', -0.5, -0.5)
-		anchor.Tooltip = 'anchor_tt'
+    if not anchor then
+        anchor = Object.Spawn('Anchor', -0.5, -0.5)
+        anchor.Tooltip = 'anchor_tt'
 
-		local warden = look(anchor, {'Warden'}, nil, 'max', true)
-		if warden then
-			anchor = warden
-		end
+        local warden = look(anchor, {'Warden'}, nil, 'max', true)
+        if warden then
+            anchor = warden
+        end
 
-		look(anchor, {'Anchor'}, deleteOldAnchor, 'max')
-		look(anchor, {'RotateObject'}, deleteOldAnchor, 'max')
-	end
+        look(anchor, {'Anchor'}, deleteOldAnchor, 'max')
+        look(anchor, {'RotateObject'}, deleteOldAnchor, 'max')
+    end
 
-	if this.TimeIndex > Ready then
-		Ready = this.TimeIndex + this.TimeWarpFactor
-		look(anchor, {'TreeStump'}, deleteO, 'max')
+    if this.TimeIndex > Ready then
+        Ready = this.TimeIndex + this.TimeWarpFactor
+        look(anchor, {'TreeStump'}, deleteO, 'max')
 
-		if this.ImmediateObjects then
-			if anchor.add_modifiers then
-				-- print('add_modifiers')
-				look(anchor, {'TimeWarpNeedModifier'}, reinstallO, 'max')
-				anchor.add_modifiers = nil
-			end
-			if anchor.remove_modifiers then
-				look(anchor, {'TimeWarpNeedModifier'}, dismantleO, 'max')
-				anchor.remove_modifiers = nil
-			end
-		end
+        if this.ImmediateObjects then
+            if anchor.add_modifiers then
+                -- print('add_modifiers')
+                look(anchor, {'TimeWarpNeedModifier'}, reinstallO, 'max')
+                anchor.add_modifiers = nil
+            end
+            if anchor.remove_modifiers then
+                look(anchor, {'TimeWarpNeedModifier'}, dismantleO, 'max')
+                anchor.remove_modifiers = nil
+            end
+        end
 
-		look(anchor, {'Polaroid', }, markPolaroid, 'max')
-		look(anchor, {'SearchToilets', }, doSearchToilets, 'max')
+        look(anchor, {'Polaroid'}, markPolaroid, 'max')
+        look(anchor, {'SearchToilets'}, doSearchToilets, 'max')
 
-		look(anchor, {'PCommand', }, exePC, 'max')
-		look(anchor, {'CCommand', }, exeCC, 'max')
-	end
+        look(anchor, {'PCommand'}, exePC, 'max')
+        look(anchor, {'CCommand'}, exeCC, 'max')
+    end
 
-	if this.ImmediateObjects and this.TimeIndex > Ready2 then
-		Ready2 = this.TimeIndex + 3 * this.TimeWarpFactor
-		look(anchor, {'Box'}, deleteMostBox, 'max')
-		look(anchor, {'Rubble'}, deleteO, 'max')
-		look(anchor, {'Stack'}, deleteMatStack, 'max')
-		-- look(anchor, {'Foreman'}, mayReinstall, 'max')
-		-- look(anchor, {'Prisoner', }, testP, 10)
+    if this.ImmediateObjects and this.TimeIndex > Ready2 then
+        Ready2 = this.TimeIndex + 3 * this.TimeWarpFactor
+        look(anchor, {'Box'}, deleteMostBox, 'max')
+        look(anchor, {'Rubble'}, deleteO, 'max')
+        look(anchor, {'Stack'}, deleteMatStack, 'max')
+        -- look(anchor, {'Foreman'}, mayReinstall, 'max')
+        -- look(anchor, {'Prisoner', }, testP, 10)
 
-	end
+    end
 end
-
-
 
 function deleteMostBox(box)
-	if box.Contents ~= 'SuperiorBed' then
-		box.Delete()
-	end
+    if box.Contents ~= 'SuperiorBed' then
+        box.Delete()
+    end
 end
-
 
 function deleteMatStack(stack)
-	if stack.Contents == 'Concrete' or stack.Contents == 'Steel' or stack.Contents == 'Brick' or stack.Contents == 'GrassTurf' then
-		stack.Delete()
-	end
+    if stack.Contents == 'Concrete' or stack.Contents == 'Steel' or stack.Contents == 'Brick' or stack.Contents ==
+        'GrassTurf' then
+        stack.Delete()
+    end
 end
-
-
 
 function mayReinstall(foreman)
 
-	if foreman.reinstall_passive then
-		look(anchor, {'Radio', 'Bin'}, reinstallO2, 'max')
-		foreman.reinstall_passive = false
-	end
+    if foreman.reinstall_passive then
+        look(anchor, {'Radio', 'Bin'}, reinstallO2, 'max')
+        foreman.reinstall_passive = false
+    end
 
 end
 
@@ -128,142 +126,130 @@ end
 -- 	p.Name = nil
 -- end
 
-
-
-
-
 local polaroid
 
 function markPolaroid(pol)
-	if pol.PolaroidIndex == -1 then
-		return
-	end
-	-- print('Found a Polaroid')
-	-- print(pol.PolaroidIndex)
-	-- print(pol.BirthTime)
-	if this.TimeIndex - pol.BirthTime > 30 then
-		pol.BirthTime = this.TimeIndex
-		polaroid = pol
-		local g = look(polaroid, {'Guard', }, nil, 20, true)
-		if g then
-			musterToPolaroid(g)
-		end
-		g = nil
-	end
+    if pol.PolaroidIndex == -1 then
+        return
+    end
+    -- print('Found a Polaroid')
+    -- print(pol.PolaroidIndex)
+    -- print(pol.BirthTime)
+    if this.TimeIndex - pol.BirthTime > 30 then
+        pol.BirthTime = this.TimeIndex
+        polaroid = pol
+        local g = look(polaroid, {'Guard'}, nil, 20, true)
+        if g then
+            musterToPolaroid(g)
+        end
+        g = nil
+    end
 
 end
 
 function musterToPolaroid(g)
-	if g.Damage < 0.1 and g.TargetObject.u <= 0 and g.Carrying.u <= 0 then
-		g.PlayerOrderPos.x, g.PlayerOrderPos.y = polaroid.Pos.x, polaroid.Pos.y
-	end
+    if g.Damage < 0.1 and g.TargetObject.u <= 0 and g.Carrying.u <= 0 then
+        g.PlayerOrderPos.x, g.PlayerOrderPos.y = polaroid.Pos.x, polaroid.Pos.y
+    end
 end
-
 
 function Init()
-	print('Inited')
+    print('Inited')
 end
-
 
 local sizeDict = {
-	AccountingTable = {3, 1},
-	Table = {4, 1},
-	Bench = {4, 1},
-	Bed = {1, 2},
-	BunkBed = {1, 2},
-	SuperiorBed = {1, 2},
-	MedicalBed = {2, 2},
-	ElectricChair = {2, 2},
-	JailDoorLarge = {2, 1},
-	WindowLarge = {2, 1},
-	Cooker = {2, 1},
-	Fridge = {2, 1},
-	Sink = {3, 1},
-	ServingTable = {5, 1},
-	OfficeDesk = {2, 1},
-	WaterBoiler = {2, 1},
-	LargeTv = {3, 1},
-	ShopFront = {2, 1},
-	CctvMonitor = {3, 1},
-	DoorControlSystem = {2, 1},
-	WeaponRack = {2, 1},
-	SofaChairDouble = {2, 1},
-	SchoolDesk = {1, 2},
-	MorgueSlab = {1, 2},
-	PowerStation = {3, 3},
-	WaterPumpStation = {3, 3},
-	Servo = {1, 2},
-	PoolTable = {3, 2},
-	WorkshopSaw = {3, 1},
-	WorkshopPress = {3, 1},
-	CarpenterTable = {3, 2},
-	VisitorTable = {3, 2},
-	VisitorTableSecure = {2, 2},
-	IroningBoard = {3, 1},
-	SortingTable = {3, 1},
-	LibraryBookshelf = {3, 1},
-	ShopShelf = {3, 1},
-	Altar = {3, 2},
-	Pews = {4, 1},
-	DogCrate = {1, 2},
-	GuardTower = {2, 2},
-	WaterBoiler = {2, 1},
+    AccountingTable = {3, 1},
+    Table = {4, 1},
+    Bench = {4, 1},
+    Bed = {1, 2},
+    BunkBed = {1, 2},
+    SuperiorBed = {1, 2},
+    MedicalBed = {2, 2},
+    ElectricChair = {2, 2},
+    JailDoorLarge = {2, 1},
+    WindowLarge = {2, 1},
+    Cooker = {2, 1},
+    Fridge = {2, 1},
+    Sink = {3, 1},
+    ServingTable = {5, 1},
+    OfficeDesk = {2, 1},
+    LargeTv = {3, 1},
+    ShopFront = {2, 1},
+    CctvMonitor = {3, 1},
+    DoorControlSystem = {2, 1},
+    WeaponRack = {2, 1},
+    SofaChairDouble = {2, 1},
+    SchoolDesk = {1, 2},
+    MorgueSlab = {1, 2},
+    PowerStation = {3, 3},
+    WaterPumpStation = {3, 3},
+    Servo = {1, 2},
+    PoolTable = {3, 2},
+    WorkshopSaw = {3, 1},
+    WorkshopPress = {3, 1},
+    CarpenterTable = {3, 2},
+    VisitorTable = {3, 2},
+    VisitorTableSecure = {2, 2},
+    IroningBoard = {3, 1},
+    SortingTable = {3, 1},
+    LibraryBookshelf = {3, 1},
+    ShopShelf = {3, 1},
+    Altar = {3, 2},
+    Pews = {4, 1},
+    DogCrate = {1, 2},
+    GuardTower = {2, 2},
+    WaterBoiler = {2, 1}
 }
 
-
 function exePC(o)
-	if o.obj_type then
-		local size = sizeDict[o.obj_type]
-		local dx = 0
-		local dy = 0
-		if size then
-			if o.Or.x == 0 then
-				dx = (size[1] / 2)
-				dy = (size[2] / 2)
-			else
-				dx = (size[2] / 2)
-				dy = (size[1] / 2)
-			end
-		end
-		WorkQueue.Request("PlaceObject", o.obj_type, o.Pos.x - dx, o.Pos.y - dy, o.Or.x, o.Or.y)
-	end
-	o.Delete()
+    if o.obj_type then
+        local size = sizeDict[o.obj_type]
+        local dx = 0
+        local dy = 0
+        if size then
+            if o.Or.x == 0 then
+                dx = (size[1] / 2)
+                dy = (size[2] / 2)
+            else
+                dx = (size[2] / 2)
+                dy = (size[1] / 2)
+            end
+        end
+        WorkQueue.Request("PlaceObject", o.obj_type, o.Pos.x - dx, o.Pos.y - dy, o.Or.x, o.Or.y)
+    end
+    o.Delete()
 end
 
-
 function doSearchToilets(o)
-	look(o, {'Toilet'}, searchO, 8)
-	o.Delete()
+    look(o, {'Toilet'}, searchO, 8)
+    o.Delete()
 end
 
 function searchO(o)
-	WorkQueue.Request("SearchObject", o)
+    WorkQueue.Request("SearchObject", o)
 end
 
-
 function exeCC(o)
-	if o.mat_type then
-		local cell = World.GetCell(o.Pos.x, o.Pos.y)
-		cell.Mat = o.mat_type_old
-		WorkQueue.Request("Construction", o.mat_type, o.Pos.x, o.Pos.y)
-	end
-	o.Delete()
+    if o.mat_type then
+        local cell = World.GetCell(o.Pos.x, o.Pos.y)
+        cell.Mat = o.mat_type_old
+        WorkQueue.Request("Construction", o.mat_type, o.Pos.x, o.Pos.y)
+    end
+    o.Delete()
 end
 
 function reinstallO(o)
-	WorkQueue.Request("PlaceObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
-	o.Delete()
+    WorkQueue.Request("PlaceObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
+    o.Delete()
 end
 
 function reinstallO2(o)
-	WorkQueue.Request("DismantleObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
-	WorkQueue.Request("PlaceObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
+    WorkQueue.Request("DismantleObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
+    WorkQueue.Request("PlaceObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
 end
-
 
 function dismantleO(o)
-	WorkQueue.Request("DismantleObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
-	o.Delete()
+    WorkQueue.Request("DismantleObject", o.Type, o.Pos.x, o.Pos.y, o.Or.x, o.Or.y)
+    o.Delete()
 end
-
 
